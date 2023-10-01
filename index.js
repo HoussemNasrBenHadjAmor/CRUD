48b03a445a218cb133dc8ee8bdca5eb2b3b6c2ca
HoussemNasrBenHadjAmor <houssembennasr10@gmail.com>
2023-09-30 23:50:04 -0400

    trying to deploy

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
-import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import todoRoute from "./routes/todos.js";
 import connectDB from "./db/index.js";
 
+connectDB();
 const app = express();
 dotenv.config();
 
@@ -17,5 +17,3 @@ app.use("/todos", todoRoute);
 app.get("/", (req, res) => {
   res.send("APP IS RUNNING");
 });
-
-connectDB();
